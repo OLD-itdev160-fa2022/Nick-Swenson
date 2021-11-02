ab6bcf4dcfb88826e7337f0a35de7846f49fd753
swenson2 <90735058+swenson2@users.noreply.github.com>
2021-11-02 10:11:52 -0500

    commit

---
+++ Project 1/js/scripts.js
@@ -0,0 +1,14 @@
+var h1product = 'New Products';
+var h2name = 'Nvidia RTX 3090, ';
+var p1description = 'The Nvidia RTX 3090 is the newest edition to the Nvidia 30 series graphics cards';
+var p2copyright ='Copyright 2021 TechReview.com';
+
+var h1productEL = document.getElementById('h1productEL');
+var h2nameEL = document.getElementById('h2nameEL');
+var p1descriptionEL = document.getElementById('p1descriptionEL');
+var p2copyrightEL = document.getElementById('p2copyrightEL');
+
+h1productEL.textContent = h1product;
+h2nameEL.textContent = h2name;
+p1descriptionEL.textContent = p1description;
+p2copyrightEL.textContent = p2copyright;
